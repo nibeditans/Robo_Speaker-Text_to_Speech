3fb59d52ed7966d092e9d347f61ce10eb6c40b4b
Nibedita Sahu <126712682+nibeditans@users.noreply.github.com>
2023-06-25 02:57:12 +0530

    Text-to-Speech
    oboSpeaker 1.1 is a Python program that utilizes the pyttsx3 library to convert text into speech. This program allows you to input text and have it spoken aloud by a virtual assistant.

---
+++ Robo-Speaker 1.1.py
@@ -0,0 +1,14 @@
+import pyttsx3
+
+if __name__ == '__main__':
+    print("Welcome to RoboSpeaker1.1. Created by NS!")
+
+    engine = pyttsx3.init()
+
+    while True:
+        x = input("Enter what do you want me to speak: ")
+        if x == "s":
+            break
+
+        engine.say(x)
+        engine.runAndWait()
